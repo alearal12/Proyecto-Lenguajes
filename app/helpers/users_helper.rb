9d52d22ab327b8bcfc9a3318f9dce8abb0fa687f
alearal12 <alejandro.araya@ucrso.info>
2022-07-08 08:20:27 -0600

    user enum working

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+    def users_type
+        User.users_type.keys.map do |user_type|
+            [User.user_type, user_type]
+        end
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+
+   enum user_type: { administrator: 0, kitchen_user: 1} 
 end
