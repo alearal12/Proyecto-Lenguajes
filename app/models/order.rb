ba3865698a2145a688aa4e8e5b2dbc812edd0387
alearal12 <alejandro.araya@ucrso.info>
2022-07-13 14:58:24 -0600

    working on filters

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
 class Order < ApplicationRecord
-
-    enum status: { pending: 0, delivered: 1} 
+    include Filterable
+    enum status: { delayed: 0, delivered: 1, in_time: 2, over_time: 3, cancelled: 4} 
 end
